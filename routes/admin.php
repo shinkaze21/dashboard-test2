b4e9bda40ade8cd95de34e8856a74183950a9fd7
BudiAnton <budi.anton21@gmail.com>
2022-05-19 14:26:59 +0700

    1st commit

---
+++ routes/admin.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(['prefix'  =>  'admin'], function () {
+
+    Route::get('login', 'Admin\LoginController@showLoginForm')->name('admin.login');
+    Route::post('login', 'Admin\LoginController@login')->name('admin.login.post');
+    Route::get('logout', 'Admin\LoginController@logout')->name('admin.logout');
+
+    Route::get('/settings', 'Admin\SettingController@index')->name('admin.settings');
+    Route::post('/settings', 'Admin\SettingController@update')->name('admin.settings.update');
+
+    Route::group(['middleware' => ['auth:admin']], function () {
+        Route::get('/', function () {
+            return view('admin.dashboard.index');
+        })->name('admin.dashboard');
+    });
+});
